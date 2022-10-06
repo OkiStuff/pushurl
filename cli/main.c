ccc7a1bc24063b5ca8956a9ff21584a3112b58d2
OkiStuff <probro1802@gmail.com>
2022-10-06 17:02:08 -0400

    switching to raw libcurl

---
+++ cli/main.c
@@ -0,0 +1,10 @@
+#include "../deps/libcurl/curl.h"
+#include "../deps/cpushover/cpushover.h"
+
+int main(void)
+{
+		curl_global_init(CURL_GLOBAL_DEFAULT); // Initialize libcurl globally
+		
+	
+		return 0;
+}
